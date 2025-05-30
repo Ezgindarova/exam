6611e505bb625fddb94825e84cef505a8e4ea7a8
Ezgindarova Varvara <varvaraezgindarova@mail.ru>
2025-05-30 10:59:13 +0300

    настройка административной части для модели

---
+++ admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import iexam
+
+@admin.register(iexam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('exam_name', 'created_at', 'exam_date', 'is_public')
+    list_filter = ('is_public', 'created_at')
+    search_fields = ('exam_name', 'users__email')
+    filter_horizontal = ('users',)
+    date_hierarchy = 'exam_date'
+    
+    fieldsets = (
+        (None, {
+            'fields': ('exam_name', 'exam_date', 'exam_image', 'is_public')
+        }),
+        ('Участники', {
+            'fields': ('users',)
+        }),
+    )
